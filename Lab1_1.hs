ae6e516ec99fa58ee3370b1203218ee7e506c9ab
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-09-04 15:49:25 +0200

    Added squared lists from exercise 2

--- Lab1_1.hs
+++ Lab1_1.hs
@@ -1,13 +1,22 @@
 import Prelude
 import Test.QuickCheck
 
-main = quickCheck prop_Compare
+main = quickCheck prop_Compare_2
+       
+prop_Compare_1 (Positive n) =
+  summedListOfNumbers n == otherListOfNumbers n
 
-prop_Compare (Positive n) =
-  summedListOfNumbers n == otherListOfNumbers a
+prop_Compare_2 (Positive n) =
+  summedSquaredListOfNumbers n == otherSquaredListOfNumbers n
 
 summedListOfNumbers :: Int -> Int
-summedListOfNumbers amount = sum $ take amount [1..]
+summedListOfNumbers n = sum $ take n [1..]
 
 otherListOfNumbers :: Int -> Int
-otherListOfNumbers amount = div (amount * (amount + 1)) 2 
+otherListOfNumbers n = flip div 2 $ (*) n $ n + 1
+
+summedSquaredListOfNumbers :: Int -> Int
+summedSquaredListOfNumbers n = sum $ map (^2) $ take n [1..]
+
+otherSquaredListOfNumbers :: Int -> Int
+otherSquaredListOfNumbers n = div (n * (n+1) * (2*n+1)) 6 
