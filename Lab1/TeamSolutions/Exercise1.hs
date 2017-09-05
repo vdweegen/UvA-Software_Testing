ee69d4c39d9d4e92e6b0150764b03805e0dff2f5
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-09-05 12:36:34 +0200

    Ignore file, merged files into a single exercise file

---
+++ Lab1/TeamSolutions/Exercise1.hs
@@ -0,0 +1,29 @@
+import Prelude
+import Test.QuickCheck
+
+-- QuickCheck implementation of Workshop 1, exercise 2
+-- time spent: 30 minutes
+-- reason: Initial trial / error of QuickCheck
+
+main :: IO()
+main = do
+  print "Checking Workshop 1, exercise 2"
+  quickCheck prop_Exercise2
+  print "Checking Workshop 1, exercise 3"
+  quickCheck prop_Exercise3
+
+prop_Exercise2 (Positive n) = summedSquaredListOfNumbers n == otherSquaredListOfNumbers n
+
+summedSquaredListOfNumbers :: Integer -> Integer
+summedSquaredListOfNumbers n = sum [ a^2 | a <- [1..n]]
+
+otherSquaredListOfNumbers :: Integer -> Integer
+otherSquaredListOfNumbers n = div (n * (n+1) * (2*n+1)) 6
+
+prop_Exercise3 (Positive n) = summedThirdPowerListOfNumbers n == otherThirdPowerListOfNumbers n
+
+summedThirdPowerListOfNumbers :: Integer -> Integer
+summedThirdPowerListOfNumbers n = sum [ a^3 | a <- [1..n]]
+
+otherThirdPowerListOfNumbers :: Integer -> Integer
+otherThirdPowerListOfNumbers n = (div (n * (n+1)) 2)^2
