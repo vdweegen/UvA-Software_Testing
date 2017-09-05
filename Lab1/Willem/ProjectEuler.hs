9cf14bb0aea0740e8a203a2f8a9fa26198a5ff0b
Willem Jan Glerum <willem-jan.glerum@lunatech.com>
2017-09-05 09:50:15 +0200

    Lab1: try project euler 9 with guards

--- Lab1/Willem/ProjectEuler.hs
+++ Lab1/Willem/ProjectEuler.hs
@@ -12,3 +12,6 @@ primes = 2 : filter prime [3..]
 
 solution10 :: Integer
 solution10 = sum ( takeWhile (< 2000000) primes )
+
+solution9 :: Integer
+solution9 = head [a * b * c | a <- [1..1000], b <- [a..1000], let c = 1000 - a -b, a^2 + b^2 == c^2]
