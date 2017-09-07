d1ef6c89b65995e4370401f48f612b929205d12b
vdweegen <cas@vdweegen.com>
2017-09-07 19:43:51 +0200

    Add **SLOW** Assignment 1 / Exercise 2

--- Lab1/Cas/1.hs
+++ Lab1/Cas/1.hs
@@ -22,7 +22,22 @@ main = do
     result1_1
     putStrLn $ "Assignment 1.2"
     result1_2
-    
+    putStrLn $ "Assignment 2"
+    result2
+    putStrLn $ "Assignment 3"
+    -- TODO
+    putStrLn $ "Assignment 4"
+    -- TODO
+    putStrLn $ "Assignment 5"
+    -- TODO
+    putStrLn $ "Assignment 6"
+    -- TODO
+    putStrLn $ "Assignment 7"
+    -- TODO
+    putStrLn $ "Assignment 8"
+    -- TODO
+    putStrLn $ "BONUS"
+    -- TODO
 
 -- Assignment 1.1 --
 base1_1 :: Integer -> Integer
@@ -37,3 +52,22 @@ induction1_2 :: Integer -> Integer
 base1_2 = \n -> sum(map(^3)[0..n])
 induction1_2 = \n -> ((n*(n+1)) `div` 2 ) ^ 2
 result1_2 = quickCheckResult(\n -> n >= 0 --> base1_2 n == induction1_2 n)
+
+-- Assignment 2 --
+base2 :: Int -> Int
+induction2 :: Int -> Int
+base2 = \n -> 2^n
+induction2 = \n -> length(subsequences[1..n])
+result2 = quickCheckResult(\n -> n >= 0 --> induction2 n == base2 n)
+
+-- Assignment 3 --
+
+-- Assignment 4 --
+
+-- Assignment 5 --
+
+-- Assignment 6 --
+
+-- Assignment 7 --
+
+-- Assignment 8 --
