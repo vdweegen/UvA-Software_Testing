6f79ff592fc36838250dbdd8b1cf31085ccc7664
Jordan Maduro <jordanmaduro@gmail.com>
2017-09-09 00:23:06 +0200

    Another one down

---
+++ Lab1/Jordan/exercise4.hs
@@ -0,0 +1,33 @@
+import Test.QuickCheck
+import Data.List
+import Prelude
+
+
+reversal :: Integer -> Integer
+reversal = read . reverse . show
+
+digits :: Integral x  => x -> [x]
+digits 0 = []
+digits x = digits (x `div` 10 ) ++ [x `mod` 10]
+
+numbers :: [Integer] -> Integer
+numbers [] = 0
+numbers (x:xs) = x * (10 ^ (length xs)) + (numbers xs)
+
+-- Given in lab
+prime :: Integer -> Bool
+prime n = n > 1 && all (\ x -> rem n x /= 0) xs
+   where xs = takeWhile (\ y -> y^2 <= n) primes
+
+primes :: [Integer]
+primes = 2 : filter prime [3..] 
+
+
+-- take 10000 primes
+isReversal x 
+    | x < 13 = False
+    | otherwise = (prime $ (numbers $ reverse (digits x))) && prime x
+
+main = do 
+    putStrLn "Primes with reversal property under 10000"
+    print $ filter isReversal $ [1..10000]
