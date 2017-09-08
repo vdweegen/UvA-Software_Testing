9f1804d8c42af5427bb4d2039f4e1012f05a740c
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-09-08 21:53:40 +0200

    first (too slow) implementation

--- Lab1/Bauke/Exercise5.hs
+++ Lab1/Bauke/Exercise5.hs
@@ -3,12 +3,27 @@ module Exercise5 where
 import Data.List
 import Test.QuickCheck
 
-main = print smallestConsecutive 5 101
+-- Implementation time: 30 minutes
+-- It's way too slow. it will find for 2, but certainly not for 101.
+-- 5 primes takes ages
+-- main issue is probably the drop & take, it re-generates the list one every iteration
 
---- Prime retriever
-primes :: [Integer]
+-- While the iteration is performed forwards, testing it would result in performing the same operation again.
+-- All combinations could be checked, but this is exactly what the algorithm does.
+-- If the result matches the new prime, it is valid
+
+main = findSum 0 101
+
+findSum :: Int -> Int -> [Int]
+findSum start amount | elem (sum $ primeList start amount) primes =  primeList start amount
+                     | otherwise = findSum (start+1) amount
+
+primeList :: Int -> Int -> [Int]
+primeList a b = drop a $ take (a+b) primes
+
+primes :: [Int]
 primes = 2 : filter prime [3..]
 
-prime :: Integer -> Bool
+prime :: Int -> Bool
 prime n = n > 1 && all (\ x -> rem n x /= 0) xs
-  where xs = takeWhile (\ y -> :ry^2 <= n) primes
+  where xs = takeWhile (\ y -> y^2 <= n) primes
