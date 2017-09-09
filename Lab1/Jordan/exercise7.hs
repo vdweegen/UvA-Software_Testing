0382d0fbfbf2ca521833d3d1f119a29afd0a462e
Jordan Maduro <jordanmaduro@gmail.com>
2017-09-09 15:18:18 +0200

    Another one done

--- Lab1/Jordan/exercise7.hs
+++ Lab1/Jordan/exercise7.hs
@@ -22,6 +22,51 @@ reverseDigits =  reverse . digits
 doubleDigits = map (sum . digits) 
 sumDoubleDigits = sum . doubleDigits 
 
-luhn x =  ((sum $ first $ reverseDigits x) +  (sumDoubleDigits $ map (*2) $ second $ reverseDigits x) ) `mod` 10 == 0
+luhnvalue x = ((sum $ first $ reverseDigits x) +  (sumDoubleDigits $ map (*2) $ second $ reverseDigits x) ) 
+luhn x =  (luhnvalue x )`mod` 10  == 0
 
+checkPrefix x y = and (zipWith (==) y (digits x))
+checkPrefixRange x range = or $ map(checkPrefix x) $ map(digits) range
+checkPrefixRanges x ranges  = or $ map(checkPrefixRange x) ranges
+checkCardFormat prefixranges numberlength x = checkPrefixRanges x prefixranges && elem (length $ digits x) numberlength
 
+isAmericanExpress, isMaster, isVisa :: Integer -> Bool
+isAmericanExpress x = checkCardFormat [[34,37]] [15] x 
+isMaster x = checkCardFormat [[51..55], [2221..2720]] [16] x
+isVisa x = checkCardFormat [[4]] [13, 16, 19] x
+
+mastercards = [
+ 5204740009900014,	
+ 5420923878724339,	
+ 5455330760000018,	
+ 5506900490000436,	
+ 5506900490000444,	
+ 5506900510000234,	
+ 5506920809243667,	
+ 5506922400634930,	
+ 5506927427317625,	
+ 5553042241984105,	
+ 5555553753048194,	
+ 5555555555554444]
+
+visas = [4012888888881881,
+ 4111111111111111,
+ 4444333322221111,
+ 4911830000000,
+ 4917610000000000,	
+ 4462030000000000,
+ 4917610000000000003]
+
+americanExpress = [
+ 371449635398431,
+ 378282246310005]
+
+main = do 
+    putStrLn "Mastercard cards"
+    print $ all isMaster mastercards   
+
+    putStrLn "AmericanExpress cards"
+    print $ all isAmericanExpress americanExpress   
+
+    putStrLn "Visa cards"
+    print $ all isVisa visas  
