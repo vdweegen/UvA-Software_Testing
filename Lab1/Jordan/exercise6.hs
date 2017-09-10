e716a0ae9f9d932a671f92c796783f92aab07ffc
Jordan Maduro <jordanmaduro@gmail.com>
2017-09-10 14:03:56 +0200

    Fixed bug

--- Lab1/Jordan/exercise6.hs
+++ Lab1/Jordan/exercise6.hs
@@ -2,6 +2,8 @@ import Test.QuickCheck
 import Data.List
 import Prelude
 
+-- 
+
 -- Given in lab
 prime :: Integer -> Bool
 prime n = n > 1 && all (\ x -> rem n x /= 0) xs
@@ -10,8 +12,8 @@ prime n = n > 1 && all (\ x -> rem n x /= 0) xs
 primes :: [Integer]
 primes = 2 : filter prime [3..] 
 
-primeProduct n = prime $ (+) 1 $ product [1..n]
+primeProduct n = prime $ (+) 1 $ product $ take n primes
 
 main = do
 -- Smallest counterexample
-    print $ head $ filter (not .primeProduct) $ [1..]
+    print $ take (head $ filter (not . primeProduct) $ [1..] ) primes
