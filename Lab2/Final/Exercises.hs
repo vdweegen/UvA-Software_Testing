474092b8906db67ca877c89558bc94488283f1cd
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-09-14 19:37:20 +0200

    merged exercises

--- Lab2/Final/Exercises.hs
+++ Lab2/Final/Exercises.hs
@@ -5,6 +5,8 @@ import Data.Char
 import System.Random
 import Test.QuickCheck
 
+import Lab2.Util.Random
+
 -- Assignment 2 / Lab 2 :: Group 14 --
 
 infix 1 -->
@@ -36,7 +38,23 @@ main = do
     exercisebonus
 
 -- Exercise 1
-exercise1 = print()
+-- QuickCheck for generating a number of random values and checking that the value is maintained
+exercise1 = quantilesIO 10000 4
+
+quantilesIO :: Int -> Int -> IO()
+quantilesIO xs q = do {
+    p <- probs xs;
+    print $ flip chi (div xs q) $ quantiles p [ fromIntegral x / (fromIntegral q) | x  <- [1..q]]
+}
+
+chi :: [Int] -> Int -> Float
+chi [] m = 0
+chi (x:xs) m = (fromIntegral((x-m)^2) / fromIntegral m) + chi xs m
+
+quantiles :: [Float] -> [Float] -> [Int]
+quantiles xs [] = []
+quantiles xs (q:qs) = [genericLength $ filter (<q) xs] ++ (quantiles (filter (>=q) xs) qs)
+
 
 -- Exercise 2
 exercise2 = print()
