78cf6f970a9c473cad43cccfdd0faa66ab61e27d
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-09-18 14:19:37 +0200

    Exercise1 entailMent, equivalence

--- Lab3/Bauke/Exercises.hs
+++ Lab3/Bauke/Exercises.hs
@@ -13,11 +13,19 @@ tautology f = all (\v -> evl v f) (allVals f)
 
 -- | logical entailment
 entails :: Form -> Form -> Bool
-entails = undefined
+entails form1 form2 = isEntailment (allResults form1, allResults form2)
+
+isEntailment :: ([Bool],[Bool]) -> Bool
+isEntailment ([],[]) = True
+isEntailment ((x:xs),(y:ys)) | x && (not y) = False
+                             | otherwise = isEntailment (xs,ys)
 
 -- | logical equivalence
 equiv :: Form -> Form -> Bool
-equiv = undefined
+equiv form1 form2 = allResults form1 == allResults form2
+
+allResults :: Form -> [Bool]
+allResults form = map (flip evl form) (allVals form)
 
 alwaysSatisfies, neverSatisfies :: Form
 alwaysSatisfies = head $ parse "+(1 -1)"
