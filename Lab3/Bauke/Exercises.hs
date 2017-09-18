266f00004beade60934494f9c72542079a013c52
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-09-18 13:21:19 +0200

    skeleton

--- Lab3/Bauke/Exercises.hs
+++ Lab3/Bauke/Exercises.hs
@@ -1,3 +1,17 @@
 module Exercises where
 
 import Lab3.Lecture3
+
+contradiction :: Form -> Bool
+contradiction = undefined
+
+tautology :: Form -> Bool
+tautology = undefined
+
+-- | logical entailment
+entails :: Form -> Form -> Bool
+entails = undefined
+
+-- | logical equivalence
+equiv :: Form -> Form -> Bool
+equiv = undefined
