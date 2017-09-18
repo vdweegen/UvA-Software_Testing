2d9b0df1fa684f9668c14c1619f3772e8f6a2bad
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-09-18 14:13:21 +0200

    Contradiction / Tautology

--- Lab3/Bauke/Exercises.hs
+++ Lab3/Bauke/Exercises.hs
@@ -2,11 +2,14 @@ module Exercises where
 
 import Lab3.Lecture3
 
+-- | There is no set of values for which the form returns true
 contradiction :: Form -> Bool
-contradiction = undefined
+contradiction form = not $ satisfiable form
 
+-- | There is no set of values for which the form returns false
 tautology :: Form -> Bool
-tautology = undefined
+-- tautology form = allOf True (map (flip evl form) (allVals form))
+tautology f = all (\v -> evl v f) (allVals f)
 
 -- | logical entailment
 entails :: Form -> Form -> Bool
@@ -15,3 +18,13 @@ entails = undefined
 -- | logical equivalence
 equiv :: Form -> Form -> Bool
 equiv = undefined
+
+alwaysSatisfies, neverSatisfies :: Form
+alwaysSatisfies = head $ parse "+(1 -1)"
+neverSatisfies = head $ parse "*(1 -1)"
+
+allOf :: Eq a => a -> [a] -> Bool
+allOf _ [] = True
+allOf a (x:xs) = a == x && allOf a xs
+
+
