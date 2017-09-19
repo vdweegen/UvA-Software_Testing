f946ea93dfdd4da27c08ed4c6438d67750ce7fd9
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-09-19 11:58:36 +0200

    added Random generator

--- Lab3/Bauke/Exercises.hs
+++ Lab3/Bauke/Exercises.hs
@@ -2,16 +2,19 @@ module Exercises where
 
 import Lab3.Lecture3
 
+import Test.QuickCheck
+import System.Random
+
 
 exercise1 = do
   putStr "alwaysTrue is a tautology: "
   print $ tautology alwaysTrue
   putStr "neverTrue is a contradiction: "
-  print $ contradiction neverTrue
+  print $ contradiction alwaysFalse
   putStr "alwaysTrue is equivalent to itself: "
   print $ equiv alwaysTrue alwaysTrue
   putStr "neverTrue is also equivalent to itself"
-  print $ equiv neverTrue neverTrue
+  print $ equiv alwaysFalse alwaysFalse
 
 -- | Exercise 1, Give the definitions for the predicates
 -- | Time spent: 30 minutes
@@ -42,9 +45,11 @@ equiv form1 form2 = allResults form1 == allResults form2
 allResults :: Form -> [Bool]
 allResults form = map (flip evl form) (allVals form)
 
-alwaysTrue, neverTrue :: Form
+alwaysTrue, alwaysFalse :: Form
 alwaysTrue = head $ parse "+(1 -1)"
-neverTrue = head $ parse "*(1 -1)"
+alwaysFalse = head $ parse "*(1 -1)"
+
+nestedExpression = head $ parse "(+(1 -1) ==> 2)"
 
 allOf :: Eq a => a -> [a] -> Bool
 allOf _ [] = True
@@ -53,3 +58,57 @@ allOf a (x:xs) = a == x && allOf a xs
 -- | Exercise 2 -
 
 -- | Exercise 3 - Convert Formulas into CNF
+
+-- | Exercise 4 - Random form generator
+-- | Generate a form => should pick an operator and then form
+
+randomOperator, randomSign, randomLiteral :: IO String
+randomOperator = randomFrom operators
+randomSign = randomFrom signs
+randomLiteral = randomFrom literals
+
+generateForm :: String -> IO String
+generateForm "<==>" = do
+                        a <- randomLiteral
+                        b <- randomLiteral
+                        return $ "(" ++ a ++ "<==>" ++ b ++ ")"
+
+randomFrom :: Eq a => [a] -> IO a
+randomFrom xs = randomInteger xs >>= (\randIndex -> return (xs !! randIndex))
+
+randomInteger :: Eq a => [a] -> IO Int
+randomInteger xs = (randomRIO (0, (length xs)-1))
+
+operators :: [String]
+operators = ["+", "*","==>","<=>"]
+
+signs :: [String]
+signs = ["", "-"]
+
+literals :: [String]
+literals = [ show a | a <- [1..1000]]
+
+-- | Exercise 5 - Bonus Exercise
+-- | Time spent: 20 minutes => no useful output, just playing with the Forms to check how to fix it
+
+type Clauses = [Clause]
+type Clause = [Int]
+
+alwaysTrueResult, alwaysFalseResult :: Clauses
+alwaysTrueResult = [[1, -1]]
+alwaysFalseResult = [[1],[-1]]
+nestedExpressionResult = [[1, -1], [2]]
+
+exercise5 = do
+  putStr "alwaysTrue in CNF: "
+  print $ alwaysTrueResult == (cnf2cls alwaysTrue)
+  putStr "alwaysFalse in CNF: "
+  print $ alwaysFalseResult == (cnf2cls alwaysFalse)
+
+cnf2cls :: Form -> Clauses
+cnf2cls form = undefined
+
+extractForms :: Form -> [Form]
+extractForms (Cnj fs) = fs
+extractForms (Dsj fs) = fs
+extractForms (Neg f) = [f]
