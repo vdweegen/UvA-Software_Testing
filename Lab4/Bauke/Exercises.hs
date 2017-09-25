28dc8e8a3cf77824b28b15e22ca1cce2853dbf99
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-09-25 15:16:25 +0200

    print some stuff at 2, quickCheck 3

--- Lab4/Bauke/Exercises.hs
+++ Lab4/Bauke/Exercises.hs
@@ -3,7 +3,11 @@ module Lab4 where
 import Data.List
 import System.Random
 
+import Lab4.SetOrd
+
 import Test.QuickCheck
+import Test.QuickCheck.Monadic
+
 -- Define Main --
 main = do
     putStrLn $ "===================="
@@ -34,20 +38,75 @@ main = do
 -- Exercise 1 :: Time spent: +-
 -- =============================================================================
 
-exercise1 = do
-  print()
+exercise1 = print ()
+
 
 -- =============================================================================
 -- Exercise 2 :: Time spent +-
 -- =============================================================================
-exercise2 = do
-  print()
+exercise2 = randomInt >>= (\n -> randomIntegers n)
+
+randomSet :: Int -> IO (Set Int)
+randomSet n = do
+                xs <- randomIntegers n
+                return $ Set xs
+
+randomIntegers :: Int -> IO [Int]
+randomIntegers n = sequence [ randomInt | a <- [1..n]]
+
+randomInt :: IO Int
+randomInt = randomRIO (0, 100)
 
 -- =============================================================================
 -- Exercise 3 :: Time spent +-
 -- =============================================================================
 exercise3 = do
-  print()
+  quickCheck prop_intersectionLength
+  quickCheck prop_differenceLength
+
+-- | Generates an intersection of two random sets
+someIntersection :: IO (Set Int)
+someIntersection = do
+            xs <- randomSet 10
+            ys <- randomSet 10
+            return $ interSection xs ys
+
+-- | Generates a difference of two random sets
+someDifference :: IO (Set Int)
+someDifference = do
+            xs <- randomSet 10
+            ys <- randomSet 10
+            return $ difference xs ys
+
+interSection, difference :: Ord a => Set a -> Set a -> Set a
+interSection (Set xs) (Set ys) = (Set (intersect xs ys))
+difference (Set xs) (Set ys) = (Set ((\\) xs ys))
+
+prop_intersectionLength (Positive n) = monadicIO $ do
+  result <- run $ validateIntersectionLength n
+  assert (True == result)
+
+validateIntersectionLength :: Int -> IO Bool
+validateIntersectionLength n = do
+  xs <- randomSet n
+  ys <- randomSet n
+  return $ (size xs) >= (size (interSection xs ys))
+
+prop_differenceLength (Positive n) = monadicIO $ do
+  result <- run $ validateDifferenceLength n
+  assert (True == result)
+
+validateDifferenceLength :: Int -> IO Bool
+validateDifferenceLength n = do
+  xs <- randomSet n
+  ys <- randomSet n
+  return $ (size xs) >= (size (difference xs ys))
+
+size :: Ord a => Set a -> Int
+size (Set xs) = length xs
+
+
+
 -- =============================================================================
 -- Exercise 4 :: Time spent +-
 -- =============================================================================
