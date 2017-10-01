5c08c1eecdd98b6dbae6ec9fdb5377397efea7f6
vdweegen <cas@vdweegen.com>
2017-10-01 13:28:30 +0200

    Add Assignment4 / Exercise 2

--- Lab4/Final/Exercises.hs
+++ Lab4/Final/Exercises.hs
@@ -2,7 +2,8 @@ module Lab4 where
 
 import Data.List
 import System.Random
-
+import Lab2.Lecture2
+import Lab4.SetOrd
 import Test.QuickCheck
 -- Define Main --
 main = do
@@ -34,7 +35,7 @@ main = do
 -- Exercise 1 :: Time spent: +-
 -- =============================================================================
 
-exercise1 = putStrLn "Read chapter 4"
+exercise1 = print $ "Read chapter 4"
 
 -- | Bauke van den Berg
 -- Time spent: 4 hours on reading / constructing the questions
@@ -46,10 +47,38 @@ exercise1 = putStrLn "Read chapter 4"
 -- TODO: Handle the exercises 4.38, proving the Theorems
 
 -- =============================================================================
--- Exercise 2 :: Time spent +-
+-- Exercise 2 :: Time spent +- 120 minutes + 20 minutes discussion
 -- =============================================================================
 exercise2 = do
-  print()
+  set <- getIntS 10 10
+  putStrLn "Manual:"
+  print set
+  putStrLn "Quickcheck:"
+  sample(arbitrary :: Gen (Set Int))
+
+-- | Re-use getIntL from Lecture2.hs and use the list2set from SetOrd.hs
+-- Time spent 10 mins
+-- Stop using the list2set and use nub and sort from Haskell
+getIntS :: Int -> Int -> IO (Set Int)
+getIntS k n = do
+  l <- getIntL k n
+  return(listToSet l)
+
+
+-- | Helper function to transform a list to a set
+-- Time spent 15 mins to remove the list2set
+listToSet :: (Eq a, Ord a) => [a] -> Set a
+listToSet l = Set (sort $ nub l)
+
+-- | Using Arbitrary from Quickcheck, based on Arbitrary instance from list
+-- Using fmap to get a set
+-- Time spent 1 hour, mostly finding out that I needed to add Ord a to the instance
+-- Bonus: this one also works for other types such as String, Char, Bool, Float, etc.
+instance (Arbitrary a, Ord a) => Arbitrary (Set a) where
+  arbitrary = sized $ \n -> do
+    k <- choose (0,n)
+    listToSet `fmap` sequence [ arbitrary | _ <- [1..k] ]
+
 
 -- =============================================================================
 -- Exercise 3 :: Time spent +-
@@ -59,7 +88,7 @@ exercise3 = do
 -- =============================================================================
 -- Exercise 4 :: Time spent +-
 -- =============================================================================
-exercise4 = putStrLn "Read Chapter 5"
+exercise4 = print $ "Read Chapter 5"
 
 -- | Bauke van den Berg
 -- Time spent: 3 hours on reading / trying out some haskell programs
