98c6c5feeef1106b1c3c88d4a6edff0b83cd0502
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-10-07 10:46:31 +0200

    added Sudoku solver to exercises

--- Lab5/Bauke/Exercises.hs
+++ Lab5/Bauke/Exercises.hs
@@ -1,6 +1,6 @@
 module Lab5 where
 
-import Lab5.Lecture5
+import Data.List
 
 -- Define Main --
 main = do
@@ -26,12 +26,218 @@ main = do
 -- Exercise 1 :: Time spent: +-
 -- =============================================================================
 
+puzzle1 :: Grid
+puzzle1 = [[0,0,0,3,0,0,0,0,0],
+           [0,0,0,7,0,0,3,0,0],
+           [2,0,0,0,0,0,0,0,8],
+           [0,0,6,0,0,5,0,0,0],
+           [0,9,1,6,0,0,0,0,0],
+           [3,0,0,0,7,1,2,0,0],
+           [0,0,0,0,0,0,0,3,1],
+           [0,8,0,0,0,4,0,0,0],
+           [0,0,2,0,0,0,0,0,0]]
+
 exercise1 = do
   putStrLn "Solution to the sudoku:"
   print $ "Fout"
 
+-- | Imported code for the solver
+
+-- | Data types
+type Node = (Sudoku,[Constraint])
+type Constraint = (Row,Column,[Value])
+type Sudoku = (Row,Column) -> Value
+type Grid   = [[Value]]
+type Value  = Int
+type Row    = Int
+type Column = Int
+
+-- | Input data
+positions, values :: [Int]
+positions = [1..9]
+values    = [1..9]
+
+blocks :: [[Int]]
+blocks = [[1..3],[4..6],[7..9]]
+
+-- | Solver
+solveAndShow :: Grid -> IO[()]
+solveAndShow gr = solveShowNs (initNode gr)
+
+-- | Convert Grid to list of constraints?
+initNode :: Grid -> [Node]
+initNode gr = let s = grid2sud gr in
+              if (not . consistent) s then []
+              else [(s, constraints s)]
+
+grid2sud :: Grid -> Sudoku
+grid2sud gr = \ (r,c) -> pos gr (r,c)
+  where
+  pos :: [[a]] -> (Row,Column) -> a
+  pos gr (r,c) = (gr !! (r-1)) !! (c-1)
+
+consistent :: Sudoku -> Bool
+consistent s = and $
+               [ rowInjective s r |  r <- positions ]
+                ++
+               [ colInjective s c |  c <- positions ]
+                ++
+               [ subgridInjective s (r,c) |
+                    r <- [1,4,7], c <- [1,4,7]]
+
+rowInjective :: Sudoku -> Row -> Bool
+rowInjective s r = injective vs where
+   vs = filter (/= 0) [ s (r,i) | i <- positions ]
+
+colInjective :: Sudoku -> Column -> Bool
+colInjective s c = injective vs where
+   vs = filter (/= 0) [ s (i,c) | i <- positions ]
+
+subgridInjective :: Sudoku -> (Row,Column) -> Bool
+subgridInjective s (r,c) = injective vs where
+   vs = filter (/= 0) (subGrid s (r,c))
+
+injective :: Eq a => [a] -> Bool
+injective xs = nub xs == xs
+
+
+constraints :: Sudoku -> [Constraint]
+constraints s = sortBy length3rd
+    [(r,c, freeAtPos s (r,c)) |
+                       (r,c) <- openPositions s ]
+
+length3rd :: (a,b,[c]) -> (a,b,[c]) -> Ordering
+length3rd (_,_,zs) (_,_,zs') = compare (length zs) (length zs')
+
+freeAtPos :: Sudoku -> (Row,Column) -> [Value]
+freeAtPos s (r,c) =
+  (freeInRow s r)
+   `intersect` (freeInColumn s c)
+   `intersect` (freeInSubgrid s (r,c))
+
+freeInRow :: Sudoku -> Row -> [Value]
+freeInRow s r =
+  freeInSeq [ s (r,i) | i <- positions  ]
+
+freeInSubgrid :: Sudoku -> (Row,Column) -> [Value]
+freeInSubgrid s (r,c) = freeInSeq (subGrid s (r,c))
+
+freeInColumn :: Sudoku -> Column -> [Value]
+freeInColumn s c =
+  freeInSeq [ s (i,c) | i <- positions ]
+
+subGrid :: Sudoku -> (Row,Column) -> [Value]
+subGrid s (r,c) =
+  [ s (r',c') | r' <- bl r, c' <- bl c ]
+
+bl :: Int -> [Int]
+bl x = concat $ filter (elem x) blocks
+
+freeInSeq :: [Value] -> [Value]
+freeInSeq seq = values \\ seq
+
+-- | Return positions with no value
+openPositions :: Sudoku -> [(Row,Column)]
+openPositions s = [ (r,c) | r <- positions,
+                            c <- positions,
+                            s (r,c) == 0 ]
+
+-- | Solve and show nodes ?
+solveShowNs :: [Node] -> IO[()]
+solveShowNs = sequence . fmap showNode . solveNs
+
+showNode :: Node -> IO()
+showNode = showSudoku . fst
+
+showSudoku :: Sudoku -> IO()
+showSudoku = showGrid . sud2grid
+
+showGrid :: Grid -> IO()
+showGrid [as,bs,cs,ds,es,fs,gs,hs,is] =
+ do putStrLn ("+-------+-------+-------+")
+    showRow as; showRow bs; showRow cs
+    putStrLn ("+-------+-------+-------+")
+    showRow ds; showRow es; showRow fs
+    putStrLn ("+-------+-------+-------+")
+    showRow gs; showRow hs; showRow is
+    putStrLn ("+-------+-------+-------+")
+
+showRow :: [Value] -> IO()
+showRow [a1,a2,a3,a4,a5,a6,a7,a8,a9] =
+ do  putChar '|'         ; putChar ' '
+     putStr (showVal a1) ; putChar ' '
+     putStr (showVal a2) ; putChar ' '
+     putStr (showVal a3) ; putChar ' '
+     putChar '|'         ; putChar ' '
+     putStr (showVal a4) ; putChar ' '
+     putStr (showVal a5) ; putChar ' '
+     putStr (showVal a6) ; putChar ' '
+     putChar '|'         ; putChar ' '
+     putStr (showVal a7) ; putChar ' '
+     putStr (showVal a8) ; putChar ' '
+     putStr (showVal a9) ; putChar ' '
+     putChar '|'         ; putChar '\n'
+
+
+showVal :: Value -> String
+showVal 0 = " "
+showVal d = show d
+
+sud2grid :: Sudoku -> Grid
+sud2grid s =
+  [ [ s (r,c) | c <- [1..9] ] | r <- [1..9] ]
+
+-- | Solve nodes ?
+solveNs :: [Node] -> [Node]
+solveNs = search succNode solved
+
+-- | No idea...
+succNode :: Node -> [Node]
+succNode (s,[]) = []
+succNode (s,p:ps) = extendNode (s,ps) p
+
+-- | Split node into multiple nodes?
+extendNode :: Node -> Constraint -> [Node]
+extendNode (s,constraints) (r,c,vs) =
+   [(extend s ((r,c),v),
+     sortBy length3rd $
+         prune (r,c,v) constraints) | v <- vs ]
+
+extend :: Sudoku -> ((Row,Column),Value) -> Sudoku
+extend = update
+
+update :: Eq a => (a -> b) -> (a,b) -> a -> b
+update f (y,z) x = if x == y then z else f x
+
+prune :: (Row,Column,Value)
+      -> [Constraint] -> [Constraint]
+prune _ [] = []
+prune (r,c,v) ((x,y,zs):rest)
+  | r == x = (x,y,zs\\[v]) : prune (r,c,v) rest
+  | c == y = (x,y,zs\\[v]) : prune (r,c,v) rest
+  | sameblock (r,c) (x,y) =
+        (x,y,zs\\[v]) : prune (r,c,v) rest
+  | otherwise = (x,y,zs) : prune (r,c,v) rest
+
+sameblock :: (Row,Column) -> (Row,Column) -> Bool
+sameblock (r,c) (x,y) = bl r == bl x && bl c == bl y
+
+
+-- | Sudoku is solved when the list of constraints is empty
+solved  :: Node -> Bool
+solved = null . snd
+
+
+search :: (node -> [node])
+       -> (node -> Bool) -> [node] -> [node]
+search children goal [] = []
+search children goal (x:xs)
+  | goal x    = x : search children goal xs
+  | otherwise = search children goal ((children x) ++ xs)
+
+-- | List of 3x3 squares to check for unique numbers
 squares :: [(Int, Int)]
-squares = [(0,0), (3,0), (6,0), (0,3), (3,3), (3,6), (0,6), (3,6), (6,6)]
+squares = [(1,1), (4,1), (7,1), (1,4), (4,4), (4,7), (1,7), (4,7), (7,7)]
 
 isValid :: Grid -> Bool
 isValid grid | not $ validGrid grid = False
@@ -64,7 +270,7 @@ compose values = [ column | step <- [1..9], let column = drop ((step-1)*9) $ tak
 
 -- | Convenience call to validate a 3x3 grid
 validSquare :: Grid -> (Int,Int) -> Bool
-validSquare grid (x,y) =  checkLine $ concat $ map (take 3) $ take 3 $ drop y $ map (drop x) grid
+validSquare grid (x,y) =  checkLine $ concat $ map (take 3) $ take 3 $ drop (y-1) $ map (drop (x-1)) grid
 
 -- =============================================================================
 -- Exercise 2 :: Time spent: +-
