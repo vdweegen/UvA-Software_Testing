f58801acc287b682f68823e36cbb9f4060227a8c
BaukeBerg <bauke.van.den.berg@gmail.com>
2017-10-09 16:17:30 +0200

    ex3 + my own lecture code

--- Lab6/Bauke/Exercises.hs
+++ Lab6/Bauke/Exercises.hs
@@ -1,6 +1,6 @@
 module Lab6 where
 
-import Lecture6
+import Bauke.Lecture6
 
 -- Define Main --
 main = do
@@ -31,6 +31,9 @@ main = do
 exercise1 = do
   print()
 
+
+exM :: Integer -> Integer -> Integer -> Integer
+exM a b c = (a^b) `mod` c
 -- =============================================================================
 -- Exercise 2 :: Time spent: +-
 -- =============================================================================
@@ -38,11 +41,20 @@ exercise2 = do
   print()
 
 -- =============================================================================
--- Exercise 3 :: Time spent: +-
+-- Exercise 3 :: Time spent: +- 5 minutes
+-- Simply write a list comprehension containing all non primes
+-- Checked the implementation against the wikipedia link for the known composites up to 150.
+-- These are exactly equal
 -- =============================================================================
 exercise3 = do
-  print()
+  putStr "Checking composites against known values up to 150: "
+  print $ verifyComposites
 
+verifyComposites :: Bool
+verifyComposites = (takeWhile (<=150) composites) == firstComposites
+
+firstComposites :: [Integer]
+firstComposites = [4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30, 32, 33, 34, 35, 36, 38, 39, 40, 42, 44, 45, 46, 48, 49, 50, 51, 52, 54, 55, 56, 57, 58, 60, 62, 63, 64, 65, 66, 68, 69, 70, 72, 74, 75, 76, 77, 78, 80, 81, 82, 84, 85, 86, 87, 88, 90, 91, 92, 93, 94, 95, 96, 98, 99, 100, 102, 104, 105, 106, 108, 110, 111, 112, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 128, 129, 130, 132, 133, 134, 135, 136, 138, 140, 141, 142, 143, 144, 145, 146, 147, 148, 150]
 -- =============================================================================
 -- Exercise 4 :: Time spent: +-
 -- =============================================================================
@@ -60,7 +72,6 @@ exercise5 = do
 -- =============================================================================
 exercise6 = do
   print()
-
 -- =============================================================================
 -- Exercise 6 (2) :: Time spent: +-
 -- =============================================================================
