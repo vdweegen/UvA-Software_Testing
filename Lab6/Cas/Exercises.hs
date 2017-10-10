8160b56a2a5280a37977088f564104e22dfaa1b0
vdweegen <cas@vdweegen.com>
2017-10-10 21:18:17 +0200

    Fix Assignment 6 / Exercise 2

--- Lab6/Cas/Exercises.hs
+++ Lab6/Cas/Exercises.hs
@@ -13,7 +13,7 @@ main = do
     putStrLn "> Exercise 1"
     -- exercise1
     putStrLn "> Exercise 2"
-    -- exercise2
+    exercise2
     putStrLn "> Exercise 3"
     -- exercise3
     putStrLn "> Exercise 4"
@@ -21,7 +21,7 @@ main = do
     putStrLn "> Exercise 5"
     -- exercise5
     putStrLn "> Exercise 6 (1)"
-    exercise6
+    -- exercise6
     putStrLn "> Exercise 6 (2)"
     -- exercise62
     putStrLn "> Exercise 7 (BONUS)"
@@ -41,12 +41,61 @@ exM x y n = t * Lab6.exM ((x * x) `mod` n) (y `div` 2) n `mod` n
 
 -- =============================================================================
 -- Exercise 2 :: Time spent: +-
+--
+-- Initial attempt:
+-- Original code 100000 primes: TimeSpec {sec = 48, nsec = 252859097}
+-- Improved code 100000 primes: TimeSpec {sec = 44, nsec = 955916568}
+--
+-- 2nd attempt:
+--
+-- Finally figured out how to compile with profiling enabled:
+-- ghc -O2 -rtsopts -prof --make Exercises.hs -main-is Lab6.main -o Exercises
+--
+-- and then execute: ./Exercises +RTS -p
+--
+-- This yields the following output:
+-- =============================================================================
+-- 	Tue Oct 10 21:11 2017 Time and Allocation Profiling Report  (Final)
+--
+-- 	   Exercises +RTS -p -RTS
+--
+-- 	total time  =        2.40 secs   (2397 ticks @ 1000 us, 1 processor)
+-- 	total alloc = 1,989,224,136 bytes  (excludes profiling overheads)
+--
+-- COST CENTRE  MODULE        SRC                                %time %alloc
+--
+-- CAF          Lecture6      <entire-module>                     97.0   96.2
+-- getStdRandom System.Random System/Random.hs:(586,1)-(587,26)    2.4    2.8
+--
+--
+--                                                                                          individual      inherited
+-- COST CENTRE   MODULE                SRC                               no.     entries  %time %alloc   %time %alloc
+--
+-- MAIN          MAIN                  <built-in>                         58          0    0.3    0.8   100.0  100.0
+--  getStdRandom System.Random         System/Random.hs:(586,1)-(587,26) 117     100000    2.4    2.8     2.4    2.8
+--  diffTimeSpec System.Clock          System/Clock.hsc:283:1-38         118          1    0.0    0.0     0.0    0.0
+--  CAF          Lab6                  <entire-module>                   115          0    0.3    0.3     0.3    0.3
+--   getTime     System.Clock          System/Clock.hsc:185:1-64         116          1    0.0    0.0     0.0    0.0
+--  CAF          Lecture6              <entire-module>                   114          0   97.0   96.2    97.0   96.2
+--  CAF          System.Random         <entire-module>                   113          0    0.0    0.0     0.0    0.0
+--  CAF          Data.Time.Clock.POSIX <entire-module>                   111          0    0.0    0.0     0.0    0.0
+--  CAF          System.Clock          <entire-module>                   110          0    0.0    0.0     0.0    0.0
+--  CAF          GHC.Conc.Signal       <entire-module>                    99          0    0.0    0.0     0.0    0.0
+--  CAF          GHC.IO.Encoding       <entire-module>                    97          0    0.0    0.0     0.0    0.0
+--  CAF          GHC.IO.Handle.FD      <entire-module>                    96          0    0.0    0.0     0.0    0.0
+--  CAF          GHC.IO.Handle.Text    <entire-module>                    95          0    0.0    0.0     0.0    0.0
+--  CAF          GHC.IO.Encoding.Iconv <entire-module>                    85          0    0.0    0.0     0.0    0.0
+--
+--
+-- Profiling results gave me a line that I needed to fix, which resulted into the
+-- following results:
+-- Improved code 100000 primes: TimeSpec {sec = 2, nsec = 651626570}
 -- =============================================================================
 exercise2 = do
-  testOriginal <- testTime $ mapM primeTestF (take 100000 primes)
-  testRefactored <- testTime $ mapM primeTest (take 100000 primes)
-  print $ "Testing 100000 primes with original code"
-  print $ testOriginal
+  -- testOriginal <- testTime $ mapM primeTestF (take 100000 primes)
+  -- print $ "Testing 100000 primes with original code"
+  -- print $ testOriginal
+  testRefactored <- testTime $ mapM Lab6.primeTest (take 100000 primes)
   print $ "Testing 100000 primes with improved code"
   print $ testRefactored
 
