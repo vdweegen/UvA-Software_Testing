502730d6dc3578248c399c22e6379f9a341704f5
Jordan Maduro <jordanmaduro@gmail.com>
2017-10-15 14:42:03 +0200

    working benchmark

--- Lab6/Jordan/Exercises.hs
+++ Lab6/Jordan/Exercises.hs
@@ -63,15 +63,14 @@ exM'' b e m = f e b 1
 -- It first generate 3 list of input and use them with both functions
 -- =============================================================================
 exercise2 = do
-    bs <- replicateM  10000000 (randomRIO (400, 10000 :: Integer))
-    es <- replicateM  10000000 (randomRIO (400, 10000 :: Integer))
-    ms <- replicateM  10000000 (randomRIO (400, 10000 :: Integer))
+    bs <- replicateM  10 (randomRIO (400, 10000 :: Integer))
+    es <- replicateM  10 (randomRIO (400, 10000 :: Integer))
+    ms <- replicateM  10 (randomRIO (400, 10000 :: Integer))
     start <- getTime Monotonic
-    print $ last $  last $ doCalculation' expM bs es ms
+    print $ doCalculation' exM'' bs es ms
     end <- getTime Monotonic
-    
     start' <- getTime Monotonic
-    print $ last $  last $ doCalculation' exM'' bs es ms
+    print $ doCalculation' expM bs es ms
     end' <- getTime Monotonic
     fprint (timeSpecs) start end
     fprint (timeSpecs) start' end'
